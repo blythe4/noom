faf7958c354dd0d1b5670e44b125438f58321550
황수영 <blythe@jstair.com>
2023-06-20 15:06:05 +0900

    server init

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+const handleListen = () => console.log("Listening on http://localhost:3000");
 
-app.listen(3000);
+app.listen(3000, handleListen);
