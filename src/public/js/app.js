312a357cdbe734f6d38c8ad16ab7289a604a42d1
황수영 <blythe@jstair.com>
2023-06-21 18:01:32 +0900

    emit last agument function

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,12 +3,15 @@ const socket = io();
 const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
 
+function backendDone(msg) {
+    console.log(`The backend says : ${msg}`);
+}
+
 function handleRoomSubmit(event) {
     event.preventDefault();
     const input = form.querySelector("input");
-    socket.emit("enter_room", { payload: input.value }, () => {
-        console.log("server is done!");
-    });
+    // emit 마지막 agument는 무조건 function 이여야함.
+    socket.emit("enter_room", input.value, backendDone);
     input.value = "";
 }
 
