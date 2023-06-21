282f578efb23822c26c6564ceacb9aac8299b09b
황수영 <blythe@jstair.com>
2023-06-21 13:58:10 +0900

    Nicknames

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,13 +1,21 @@
-const messgaeList = document.querySelector("ul");
-const messgaeFrom = document.querySelector("form");
+const messageList = document.querySelector("ul");
+const messageFrom = document.querySelector("#message");
+const nickFrom = document.querySelector("#nick");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMesage(type, payload) {
+    const msg = { type, payload };
+    return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
     console.log("Connected to Server ✅");
 });
 
 socket.addEventListener("message", (message) => {
-    console.log("New message: ", message.data);
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
@@ -16,9 +24,16 @@ socket.addEventListener("close", () => {
 
 function handleSubmit(event) {
     event.preventDefault();
-    const input = messgaeFrom.querySelector("input");
-    socket.send(input.value);
+    const input = messageFrom.querySelector("input");
+    socket.send(makeMesage("new_message", input.value));
     input.value = "";
 }
 
-messgaeFrom.addEventListener("submit", handleSubmit);
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickFrom.querySelector("input");
+    socket.send(makeMesage("nickname", input.value));
+    input.value = "";
+}
+messageFrom.addEventListener("submit", handleSubmit);
+nickFrom.addEventListener("submit", handleNickSubmit);
