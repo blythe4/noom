ccbc754228dbafd914bdfc19bc7ac05f7995a3e9
황수영 <blythe@jstair.com>
2023-06-27 15:42:40 +0900

    WebRTC STUN

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -144,7 +144,19 @@ socket.on("ice", (ice) => {
 
 // RTC Code
 function makeConnection() {
-    myPeerConeection = new RTCPeerConnection();
+    myPeerConeection = new RTCPeerConnection({
+        iceServers: [
+            {
+                urls: [
+                    "stun:stun.l.google.com:19302",
+                    "stun:stun1.l.google.com:19302",
+                    "stun:stun2.l.google.com:19302",
+                    "stun:stun3.l.google.com:19302",
+                    "stun:stun4.l.google.com:19302",
+                ],
+            },
+        ],
+    });
     myPeerConeection.addEventListener("icecandidate", handleIce);
     myPeerConeection.addEventListener("addstream", handleAddStream);
     myStream.getTracks().forEach((track) => myPeerConeection.addTrack(track, myStream));
